e93068942638ddaccbb8b92715645e56e24d88b7
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-02-26 22:24:48 +0000

    Added Basic Test Class

---
+++ src/com/beanexpert/selenium/JUnitExample.java
@@ -0,0 +1,24 @@
+package com.beanexpert.selenium;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class JUnitExample {
+
+    @BeforeEach
+    public void setUp(){
+
+    }
+
+    @Test
+    public void testGoogle(){
+
+    }
+
+    @AfterEach
+    public void tearDown(){
+
+    }
+
+}

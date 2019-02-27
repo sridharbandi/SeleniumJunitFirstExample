a088f4b608c72f9630e6c7991fd5927499d3f493
Sridhar.Bandi <sridhar.bandi.ece@gmail.com>
2019-02-27 20:59:10 +0000

    Updated Tests

--- src/test/JunitExample.java
+++ src/test/JunitExample.java
@@ -1,2 +1,48 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class JunitExample {
+
+    WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        //Set the path of chromedriver. replace with chromedriver path
+        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
+        //ChromeDriver instantiation
+        driver = new ChromeDriver();
+        //Maximizes the browser
+        driver.manage().window().maximize();
+        //Open the URL
+        driver.get("http://www.google.com/");
+    }
+
+    @Test
+    public void verifyTitle() {
+        //Verify the title is Google
+        assertEquals("Google", driver.getTitle());
+    }
+
+    @Test
+    public void searchSelenium() {
+        //WebElement instantiation
+        WebElement searchBox = driver.findElement(By.name("q"));
+        //Type Selenium
+        searchBox.sendKeys("Selenium");
+        //Simulate Hit Keyboard Enter
+        searchBox.submit();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        //Close the browser
+        driver.quit();
+    }
 }
